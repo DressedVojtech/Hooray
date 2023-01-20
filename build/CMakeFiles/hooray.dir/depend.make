6e79c53312645efb31825add6f06ce7065457c47
Vojtěch Kosina <vojtech.kosina@email.cz>
2023-01-20 20:06:28 +0100

    Remove build directory

--- build/CMakeFiles/hooray.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for hooray.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/hooray.dir/src/rgb.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/hooray.dir/src/rgb.cpp.o: /home/vk/dev/cpp/Hooray/src/rgb.cpp \
- /usr/include/stdc-predef.h /home/vk/dev/cpp/Hooray/src/rgb.h

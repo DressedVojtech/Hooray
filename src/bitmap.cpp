63c4568e0a18019b5c0709c7e96c3987720fd26e
Vojtěch Kosina <vojtech.kosina@email.cz>
2023-01-20 20:04:56 +0100

    Done

--- src/bitmap.cpp
+++ src/bitmap.cpp
@@ -1,14 +1,11 @@
 #include "bitmap.h"
 #include <vector>
+#include "rgb.h"
 
-bitmap(int sizeX, int sizeY) {
-    for (int i = 0; i < sizeX; i++) {
-        for (int j = 0; j < sizeY; j++) {
-            bm[i][j]= {0, 0, 0};
-        }
-    }
+bitmap::bitmap(int width, int height) {
+    bm = std::vector<std::vector<rgb>>(height, std::vector<rgb>(width, {0, 0, 0}));
 }
 
-void setCell(rgb color, int i, int j) {
+void bitmap::setCell(rgb color, int i, int j) {
     bm[i][j] = color;
 }
